f44833c577ae143855e0b122946cea46be1ebb61
Chris Williams <orktagonapus@gmail.com>
2023-12-09 17:45:04 +0000

    Prepare for day 5

---
+++ day5/src/main/scala/advent/of/code/day5/DayFive.scala
@@ -0,0 +1,19 @@
+package advent.of.code.day5
+
+import advent.of.code.utils.Solution
+import cats.effect.IOApp
+
+import scala.annotation.tailrec
+
+
+object DayFive extends Solution with IOApp.Simple {
+
+  lazy val day = 5
+
+  def solve(puzzleInput: String): String = {
+    val partOne = "" //solvePartOne(puzzleInput)
+    val partTwo = "" //solvePartTwo(puzzleInput)
+    "Part 1 solution: " ++ partOne ++ "\nPart 2 solution: " ++ partTwo
+  }
+
+}
