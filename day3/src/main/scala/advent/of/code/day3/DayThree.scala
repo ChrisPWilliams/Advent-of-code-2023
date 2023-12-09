1615b34abdd41f096f3bd3d6f4e6852cdfe9ea6f
Chris Williams <orktagonapus@gmail.com>
2023-12-09 15:31:17 +0000

    Solve day 3 part 2 (messy but it works)

--- day3/src/main/scala/advent/of/code/day3/DayThree.scala
+++ day3/src/main/scala/advent/of/code/day3/DayThree.scala
@@ -4,6 +4,7 @@ import advent.of.code.utils.Solution
 import cats.effect.IOApp
 
 import scala.annotation.tailrec
+import scala.util.matching.Regex
 
 
 object DayThree extends Solution with IOApp.Simple {
@@ -12,10 +13,17 @@ object DayThree extends Solution with IOApp.Simple {
 
   def solve(puzzleInput: String): String = {
     val partOne = solvePartOne(puzzleInput)
-    val partTwo = "" //solvePartTwo(puzzleInput)
+    val partTwo = solvePartTwo(puzzleInput)
     "Part 1 solution: " ++ partOne ++ "\nPart 2 solution: " ++ partTwo
   }
 
+  object Regexes {
+    val GetFullNum: Regex = "[0-9]+".r
+    val p1GetSymbol: Regex = "[^a-zA-Z0-9.]".r
+    val p2ForwardAdjacentNum: Regex = "..[0-9]".r
+    val p2BackwardAdjacentNum: Regex = "[0-9]..".r
+    val p2GetNum: Regex = "(?<=[0-9])[0-9]*".r
+  }
 
   def getHeightAndWidthAndBuffer(input: String): (Int, Int, String) = {
     val rows = input.split("\n")
@@ -62,11 +70,11 @@ object DayThree extends Solution with IOApp.Simple {
     } else {
       val char = rows(width + startX)
       if (char.isDigit) {
-        val number = "[0-9]+".r.findFirstIn(rows.slice(width + startX, width * 2)).get
+        val number = Regexes.GetFullNum.findFirstIn(rows.slice(width + startX, width * 2)).get
         val checkSpace = rows.slice(startX-1, startX+1 + number.length) ++
           rows.slice(startX-1 + width, startX+1 + width + number.length) ++
           rows.slice(startX-1 + width * 2, startX+1 + number.length + width * 2)
-        "[^a-zA-Z0-9.]".r.findFirstIn(checkSpace) match {
+        Regexes.p1GetSymbol.findFirstIn(checkSpace) match {
           case None => addPartsInRow(
             startX = startX + number.length,
             rows = rows,
@@ -99,6 +107,125 @@ object DayThree extends Solution with IOApp.Simple {
       parts = Seq.empty
     )
     parts.sum.toString
+  }
+
+
+  @tailrec
+  def getGearRatios(
+                     startX: Int,
+                     startY: Int,
+                     schematic: String,
+                     gears: Seq[Int],
+                     width: Int,
+                     height: Int
+                   ): Seq[Int] = {
+    if (startX % width == width - 1) {
+      if (startY == height - 2) {
+        gears
+      } else {
+        getGearRatios(
+          startX = 1,
+          startY = startY + 1,
+          schematic = schematic,
+          gears = gears,
+          width = width,
+          height = height
+        )
+      }
+    } else {
+      val char = schematic(width * startY + startX)
+      if (char == '*') {
+        val adjacentTop = schematic.slice((startX - 1) + (startY - 1) * width, (startX + 2) + (startY - 1) * width)
+        val adjacentMid = schematic.slice((startX - 1) + startY * width, (startX + 2) + startY * width)
+        val adjacentBottom = schematic.slice((startX - 1) + (startY + 1) * width, (startX + 2) + (startY + 1) * width)
+
+        val topRowForward = Regexes.p2ForwardAdjacentNum.findFirstIn(adjacentTop) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice((startX + 1) + (startY - 1) * width, startY * width)
+          ).get
+        }
+        val topRowBackward = Regexes.p2BackwardAdjacentNum.findFirstIn(adjacentTop) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice((startY - 1) * width, (startY - 1) * width + startX).reverse
+          ).get.reverse
+        }
+        val midRowForward = Regexes.p2ForwardAdjacentNum.findFirstIn(adjacentMid) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice((startX + 1) + startY * width, (startY + 1) * width)
+          ).get
+        }
+        val midRowBackward = Regexes.p2BackwardAdjacentNum.findFirstIn(adjacentMid) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice(startY * width, startY * width + startX).reverse
+          ).get.reverse
+        }
+        val bottomRowForward = Regexes.p2ForwardAdjacentNum.findFirstIn(adjacentBottom) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice((startX + 1) + (startY + 1) * width, (startY + 2) * width)
+          ).get
+        }
+        val bottomRowBackward = Regexes.p2BackwardAdjacentNum.findFirstIn(adjacentBottom) match {
+          case None => ""
+          case _ => Regexes.p2GetNum.findFirstIn(
+            schematic.slice((startY + 1) * width, (startY + 1) * width + startX).reverse
+          ).get.reverse
+        }
+        val topRowFull = topRowBackward ++ adjacentTop ++ topRowForward
+        val midRowFull = midRowBackward ++ adjacentMid ++ midRowForward
+        val bottomRowFull = bottomRowBackward ++ adjacentBottom ++ bottomRowForward
+
+        val matches: List[String] = Regexes.GetFullNum.findAllIn(topRowFull).toList ++
+          Regexes.GetFullNum.findAllIn(midRowFull).toList ++
+          Regexes.GetFullNum.findAllIn(bottomRowFull).toList
+        if (matches.length == 2) {
+          val ratio = matches.map(_.toInt).product
+          getGearRatios(
+            startX = startX + 1,
+            startY = startY,
+            schematic = schematic,
+            gears = ratio +: gears,
+            width = width,
+            height = height
+          )
+        } else {
+          getGearRatios(
+            startX = startX + 1,
+            startY = startY,
+            schematic = schematic,
+            gears = gears,
+            width = width,
+            height = height
+          )
+        }
+      } else {
+        getGearRatios(
+          startX = startX + 1,
+          startY = startY,
+          schematic = schematic,
+          gears = gears,
+          width = width,
+          height = height
+        )
+      }
+    }
+  }
+
+  def solvePartTwo(puzzleInput: String): String = {
+    val (height, width, bufferedSchematic) = getHeightAndWidthAndBuffer(puzzleInput)
+    val gearRatios = getGearRatios(
+      height = height,
+      width = width,
+      schematic = bufferedSchematic,
+      startX = 1,
+      startY = 1,
+      gears = Seq.empty
+    )
+    gearRatios.sum.toString
   }
 
 }
